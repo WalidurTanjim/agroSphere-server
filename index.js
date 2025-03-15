f9a9e306af533ad81fb5b7a8503931eff4c63e5f
Md-Mueid-Mia <mdmueidmia@gmail.com>
2025-03-15 22:53:37 +0600

    Mongodb Setup done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+app.use(cors({origin: ['http://localhost:5173',],credentials: 'true'}));
 app.use(express.json());
 
 
