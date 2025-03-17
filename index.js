5068eab1530b9b3d9791853d1a990606f283c7a8
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-03-17 23:58:02 +0600

    server updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
 };
