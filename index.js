3774fdf371ee5fb4f86338861b21f49c5f19e6d9
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-03-24 01:11:28 +0600

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://nimble-souffle-58fb98.netlify.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
