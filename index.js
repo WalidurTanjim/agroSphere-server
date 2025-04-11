0ec98850c85113b370c1497527674a19da0f49df
Md-Mueid-Mia <mdmueidmia@gmail.com>
2025-04-11 20:53:27 +0600

    update code

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // database info
     const db = client.db("agroSphere");
